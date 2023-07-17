01308b80d81576b1aa0d80d516a4183fe9e178c1
GiantPenguins <80788588+GiantPenguins@users.noreply.github.com>
2023-07-17 09:28:48 +0100

    Update JavaScript
    Update Javascript.
    Add DomContentLoaded to remove null error.

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-const answers = [
-"Yes",
-"No",
-"Maybe"
-];
+document.addEventListener('DOMContentLoaded', function() {
+  const answers = [
+    "Yes",
+    "No",
+    "Maybe"
+  ];
 
-const resultElement = document.getElementById("result");
-const shakeButton = document.getElementById("shake-button");
+  const resultElement = document.getElementById("result");
+  const shakeButton = document.getElementById("shake-button");
 
-function generateAnswer() {
-  const randomIndex = Math.floor(Math.random() * answers.length);
-  return answers[randomIndex];
-}
-
-function handleShakeButtonClick() {
-  if (resultElement && shakeButton) {
-    const answer = generateAnswer();
-    console.log("Hello World!");
-    resultElement.textContent = answer;
-  } else {
-    console.error("Element not found.");
+  function generateAnswer() {
+    const randomIndex = Math.floor(Math.random() * answers.length);
+    return answers[randomIndex];
   }
-}
 
-if (shakeButton) {
-  shakeButton.addEventListener("click", handleShakeButtonClick);
-} else {
-  console.error("Button element not found.");
-}
+  function handleShakeButtonClick() {
+    if (resultElement && shakeButton) {
+      const answer = generateAnswer();
+      console.log("Hello World!");
+      resultElement.textContent = answer;
+    } else {
+      console.error("Element not found.");
+    }
+  }
+
+  if (shakeButton) {
+    shakeButton.addEventListener("click", handleShakeButtonClick);
+  } else {
+    console.error("Button element not found.");
+  }
+});
